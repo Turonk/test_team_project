3a699acb5fbe9c4d5a00f34f88c3a5fe791ff5c6
Andrew Gorlov <Gorlov.a@mail.ru>
2022-12-25 10:43:31 +0300

    add model Topic

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    title = models.CharField(max_length=200,verbose_name=('Тема'),null=True,)
+    description = models.TextField(verbose_name=('Описание'), blank=True, null=True, )
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = 'Тему'
+        verbose_name_plural = 'Темы'
