048064925803e7b09bf60601f6c1ea835a120dc1
Andrew Gorlov <Gorlov.a@mail.ru>
2022-12-25 10:47:49 +0300

    add model Invoice_position

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Invoice_position(models.Model):
+    invoce_name = models.CharField(max_length=200,verbose_name=('Наименование по накладной'))
+    invoce_number = models.CharField(max_length=50,verbose_name=('Номер накладной'))
+    pub_data = models.DateTimeField(verbose_name=('Дата накладной'))
+    count = models.IntegerField(default=1, verbose_name=('Количество'))
+    stock_position = models.ForeignKey(
+        Part,
+        verbose_name="Позиция на складе",
+        on_delete=models.CASCADE,
+    )
+    unit_price = models.IntegerField(default=1, verbose_name='Цена за единицу, руб')
+    summa = models.IntegerField(default=1, verbose_name=('Сумма'))
+    manufacturer = models.CharField(max_length=300, verbose_name=('Производитель'))
+
+    def __str__(self):
+        return self.manufacturer
+
+    class Meta:
+        verbose_name = 'Позиция по накладной'
+        verbose_name_plural = 'Позиции по накладным'
